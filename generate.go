a406cf6fb0d3ad92a820794c3fd306d73c94e6de
lissteron <lissteron@gmail.com>
2020-12-13 12:55:04 +0300

    add generate

--- generate.go
+++ generate.go
@@ -1,5 +1,4 @@
 package dbhook
 
-// nolint:godox,lll // make it later
-// TODO
-// mockgen -destination mocks/sql.go -package mocks database/sql/driver Conn,ConnBeginTx,Driver,Stmt,Tx,ExecerContext,QueryerContext,Rows,Result
+//nolint:lll // generate ok
+//go:generate mockgen -destination mocks/sql.go -package mocks database/sql/driver Conn,ConnBeginTx,Driver,Stmt,Tx,ExecerContext,QueryerContext,Rows,Result
